a51216aff9d7d1ca285eb85221934e1e075f6980
Giacomo <giacomo.verdicchio@mail.polimi.it>
2022-03-26 17:02:15 +0100

    Commit

---
+++ src/test/java/model/cards/AssistantCardTest.java
@@ -0,0 +1,21 @@
+package model.cards;
+
+import org.junit.*;
+import org.junit.Test;
+
+
+public class AssistantCardTest {
+
+
+    @Test
+    public void TestGetMovement()
+    {
+
+    }
+
+    @Test
+    public void TestGetValue()
+    {
+
+    }
+}
