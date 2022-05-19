cf13f898e3ef8aa7ef5b15af32aa1534daec14d3
Giacomo <giacomo.verdicchio@mail.polimi.it>
2022-05-19 16:38:36 +0200

    Add footprints of other classes

---
+++ src/main/java/Client/GUI/Controllers/IntroController.java
@@ -0,0 +1,33 @@
+package Client.GUI.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class IntroController {
+    @FXML public Button Start;
+    @FXML public Button Close;
+    @FXML public Button Story;
+
+    public void oncClickExitButton(ActionEvent actionEvent)
+    {
+        Stage stage=(Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    public void onClickStart(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/GUI/Controllers/Login.fxml"));
+
+        Stage stage=(Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene= new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+}
