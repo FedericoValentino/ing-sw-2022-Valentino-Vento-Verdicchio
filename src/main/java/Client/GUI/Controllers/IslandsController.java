187b32db6e5ae90e06b6789cf99f216ca8a256b4
Giacomo <giacomo.verdicchio@mail.polimi.it>
2022-06-01 18:16:31 +0200

    Refactor resource's folder

--- src/main/java/Client/GUI/Controllers/IslandsController.java
+++ src/main/java/Client/GUI/Controllers/IslandsController.java
@@ -1,13 +1,21 @@
 package Client.GUI.Controllers;
 
+import Client.LightView.LightIslands;
+import Observer.ObserverLightView;
 import javafx.scene.layout.AnchorPane;
 
-public class IslandsController extends Controller{
+public class IslandsController extends Controller implements ObserverLightView {
 
     public AnchorPane cloudsAnchorPane;
 
-    public void setup(AnchorPane anchorPane)
+    public void setup(AnchorPane anchorPane, LightIslands lightIslands)
     {
+        lightIslands.addObserverLight(this);
+        //Con il mio anchor pane  poi vado a settare la
+    }
+
+    @Override
+    public void update(Object o) {
 
     }
 }
