9ddaf938d6471eab1161659bf94980f70652dea0
FedericoValentino <10679810@polimi.it>
2022-06-07 23:12:37 +0200

    Island

--- src/main/java/Client/GUI/Controllers/IslandController.java
+++ src/main/java/Client/GUI/Controllers/IslandController.java
@@ -3,15 +3,17 @@ package Client.GUI.Controllers;
 import Client.GUI.GuiMainStarter;
 import Client.LightView.LightIsland;
 import Client.LightView.LightMotherNature;
+import Client.LightView.LightSchool;
 import Client.Messages.ActionMessages.MoveMN;
 import Client.Messages.SerializedMessage;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import model.boards.token.Col;
-
+import model.boards.token.ColTow;
 
 
 public class IslandController extends Controller{
@@ -21,9 +23,11 @@ public class IslandController extends Controller{
     @FXML public Text pinkStudents;
     @FXML public Text yellowStudents;
     @FXML public Text greenStudents;
+    @FXML public Text towerNumber;
     @FXML public ImageView motherNature;
     @FXML public ImageView noEntry;
     @FXML public AnchorPane islandPane;
+    @FXML public Pane ownerShip;
 
     private int islandID;
     private LightMotherNature MN;
@@ -44,8 +48,25 @@ public class IslandController extends Controller{
         GuiMainStarter.getClientGUI().getServerConnection().sendMessage(new SerializedMessage(new MoveMN(amount)));
     }
 
+    public String getTowerColorPath(ColTow color)
+    {
+        switch(color)
+        {
+            case WHITE:
+                return "/Client/GUI/Images/Tower/white_tower.png";
+            case BLACK:
+                return "/Client/GUI/Images/Tower/black_tower.png";
+            case GREY:
+                return "/Client/GUI/Images/Tower/grey_tower.png";
+            default:
+                return "";
+        }
+    }
+
     public void setup(LightIsland island, int ID, LightMotherNature Mother, int total)
     {
+        towerNumber.setVisible(false);
+        ownerShip.setVisible(false);
         this.MN = Mother;
         islandID = ID;
         this.totalIslands = total;
@@ -57,10 +78,16 @@ public class IslandController extends Controller{
         motherNature.setVisible(island.isMotherNature());
         noEntry.setVisible(island.isNoEntry());
         islandPane.setOnMouseClicked(this::onClick);
-    }
-
-    public void randomImage()
-    {
+        if(island.getOwnership() != null)
+        {
+            ownerShip.getChildren().clear();
+            ImageView ownershipImage = new ImageView(getTowerColorPath(island.getOwnership()));
+            ownershipImage.setFitWidth(21);
+            ownershipImage.setFitHeight(21);
+            ownerShip.getChildren().add(ownershipImage);
+            towerNumber.setText(Integer.toString(island.getTowerNumber()));
+            towerNumber.setVisible(true);
+        }
 
     }
 }
