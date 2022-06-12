130fbad2c3af84bef62a514468120c075cd1faa5
EnricoVento <enricoalessandro.vento@mail.polimi.it>
2022-06-12 23:47:54 +0200

    First implementation of new CLI character handling

--- src/main/java/it/polimi/ingsw/Client/CLI/CharacterActivationParser.java
+++ src/main/java/it/polimi/ingsw/Client/CLI/CharacterActivationParser.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.Client.CLI;
 
+import it.polimi.ingsw.Client.Messages.ActionMessages.PlayCharacter;
 import it.polimi.ingsw.Client.Messages.ActionMessages.PlayCharacterEffect;
 import it.polimi.ingsw.model.boards.token.CharacterName;
 import it.polimi.ingsw.model.boards.token.Col;
